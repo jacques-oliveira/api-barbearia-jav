a34f8460ba3ddc0af1fa77fd0a1d561e702e64b6
jacques-oliveira <jacques.oliveira.eng@gmail.com>
2024-10-09 22:19:37 -0300

    Service - Obter todos endereços

--- src/main/java/com/barbearia/api/service/EnderecoService.java
+++ src/main/java/com/barbearia/api/service/EnderecoService.java
@@ -1,4 +1,38 @@
 package com.barbearia.api.service;
 
-public class EnderecoService {
+import com.barbearia.api.Repository.EnderecoRepository;
+import com.barbearia.api.model.Endereco;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EnderecoService implements IEnderecoService{
+
+    @Autowired
+    private EnderecoRepository _enderecoRepository;
+
+    public EnderecoService(EnderecoRepository enderecoRepository){
+        this._enderecoRepository = enderecoRepository;
+    }
+    @Override
+    public List<Endereco> findAll() {
+        return _enderecoRepository.findAll();
+    }
+
+    @Override
+    public Endereco findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Endereco create(Endereco endereco) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return false;
+    }
 }

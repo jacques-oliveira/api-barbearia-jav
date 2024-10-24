fe6b5ba9b2b480237fb6001e90a1a558e3874007
jacques-oliveira <jacques.oliveira.eng@gmail.com>
2024-10-24 14:23:02 -0300

    Get AgendamentoService

--- src/main/java/com/barbearia/api/service/AgendamentoService.java
+++ src/main/java/com/barbearia/api/service/AgendamentoService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class AgendamentoService implements {
+public class AgendamentoService implements IAgendamentoService{
     @Autowired
     private AgendamentoRepository _agendamentoRepositoy;
 
@@ -16,4 +16,23 @@ public class AgendamentoService implements {
         _agendamentoRepositoy = agendamentoRepository;
     }
 
+    @Override
+    public Agendamento findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Agendamento> findAll(){
+        return _agendamentoRepositoy.findAll();
+    }
+
+    @Override
+    public Agendamento create(Agendamento agendamento) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return false;
+    }
 }

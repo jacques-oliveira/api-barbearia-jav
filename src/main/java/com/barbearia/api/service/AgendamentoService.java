2f4d2eecbc8365c34baef2cf8c781ef3b432500f
jacques-oliveira <jacques.oliveira.eng@gmail.com>
2024-10-24 14:21:04 -0300

    IAgendamentoService

--- src/main/java/com/barbearia/api/service/AgendamentoService.java
+++ src/main/java/com/barbearia/api/service/AgendamentoService.java
@@ -1,15 +1,19 @@
 package com.barbearia.api.service;
 
 import com.barbearia.api.Repository.AgendamentoRepository;
+import com.barbearia.api.model.Agendamento;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class AgendamentoService {
+public class AgendamentoService implements {
     @Autowired
     private AgendamentoRepository _agendamentoRepositoy;
 
     public AgendamentoService(AgendamentoRepository agendamentoRepository){
         _agendamentoRepositoy = agendamentoRepository;
     }
+
 }
